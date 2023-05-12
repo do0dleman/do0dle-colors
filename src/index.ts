d347d10aa301f8c4a38b08a4405e63eaa0a2e362
Nikita <nikita.obrazcovs@gmail.com>
2023-05-12 20:17:39 +0300

    fixed npm issues

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import generateColors from "./generateColors";
 import Color from "./color/color";
 import genMethodMap from "./genMethodMap";
 
-module.exports = { generateColors, Color, genMethodMap }
+export { generateColors, Color, genMethodMap }
