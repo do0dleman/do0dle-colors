43df9591794c9e47e92580ca0466692ba8e5539f
do0dleman <97869897+do0dleman@users.noreply.github.com>
2023-11-14 19:26:51 +0200

    added conversion function exports

---
+++ src/conversionFunctions/_conversionFunctions.ts
@@ -0,0 +1,11 @@
+import hexToRgb from "./hexToRgb";
+import hslToRgb from "./hslToRgb";
+import okLabToRgb from "./OkLabToRGb";
+import OkLChToRgb from "./OkLChToRgb";
+import OkLChToRgbRough from "./OkLChtoRgbRough";
+import rgbToHex from "./rgbToHex";
+import rgbToHsl from "./rgbToHsl";
+import rgbToOkLab from "./rgbToOkLab";
+import rgbToOkLCh from "./rgbToOkLCh";
+
+export { hexToRgb, hslToRgb, OkLChToRgb, OkLChToRgbRough, okLabToRgb, rgbToHex, rgbToHsl, rgbToOkLCh, rgbToOkLab }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Color } from "./color/color.js";
 import genMethods from "./color/genMethods.js";
 import genMethod from "./types/genMethod.js";
+import * as ConversionFunctions from "./conversionFunctions/_conversionFunctions.js";
 
-export { Color, genMethods }
+export { Color, genMethods, ConversionFunctions }
 export type { genMethod }
